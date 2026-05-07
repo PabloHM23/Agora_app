d4948028d43a84feb6bdf0e16f7f98a34753e31c
Pablo Hernández <141796387+PabloHM23@users.noreply.github.com>
2026-05-07 12:35:00 -0700

    add more change

--- app/src/main/java/com/example/agora/MainActivity.kt
+++ app/src/main/java/com/example/agora/MainActivity.kt
@@ -6,14 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-<<<<<<< Updated upstream
-=======
 import androidx.compose.material3.CenterAlignedTopAppBar
->>>>>>> Stashed changes
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,11 +25,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     topBar = {
-<<<<<<< Updated upstream
-                        TopAppBar(
-=======
                         CenterAlignedTopAppBar(
->>>>>>> Stashed changes
                             title = {
                                 Text("Agora")
                             }
